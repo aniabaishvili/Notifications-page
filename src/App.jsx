abfc22d1eee9518492a9e18ec79d7088067ba0fb
ani <aniabaishvili1@gmail.com>
2024-03-08 00:58:04 +0400

    add data and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,61 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
-
+import data from '../src/components/data.json'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState(data)
 
   return (
    <div className='container'>
     <Header/>
+    <div className='notif-div'>
+      {userData.map((item) => {
+        return(
+          <div>
+          <img id="image"src={`./assets/avatar-${item.author.replace(" ", "-").toLocaleLowerCase()}.webp`}/>
+          <div>
+          <p className='notif-text'>
+            <span id="author">{item.author}</span>
+            {" "}
+         
+            <span style={styles.type}>
+              {item.type}
+               </span>
+
+          {/* <span>{item.type}</span> */}
+            {" "}
+            {item.content.includes(".webp") ? (
+            <img src={item.content} alt="Notification Content" />
+              ) : (
+              
+             <span style={{ color: item.type === "Left the group" || item.type === "has joined your group" 
+               ? "#0A327B"  : "#5E6778", fontWeight: 600  }}>
+                 {item.content}
+                   </span>
+                  )}
+                 
+
+            <p id='time'>{item.time}</p>
+          </p>
+          </div>
+        </div>
+        
+        )
+      })}
+    </div>
    </div>
    
   
      
   )
 }
+const styles ={
+  type: {
+  color: "#5E6778",
+  }
+}
+
 
 export default App
