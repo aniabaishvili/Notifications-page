f0d9b12e687b67d8772e9a5132a0022ab87a1e9a
ani <aniabaishvili1@gmail.com>
2024-03-11 20:00:54 +0400

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,34 @@ import './App.css'
 import Header from './components/Header/Header'
 import data from '../src/components/data.json'
 
+const markAsRead = () => {
+  const clone = userData.map(item => {
+    item.read = true;
+    return item;
+  })
+  console.log(clone);
+}
+
+
+
+
 
 function App() {
   const [userData, setUserData] = useState(data)
 
+
   return (
+  
    <div className='container'>
     <Header/>
-    <div className="notif-div">
+    <div  className="notif-div">
       {userData.map((item) => {
         return(
           <div>
           <img id="image"src={`./assets/avatar-${item.author.replace(" ", "-").toLocaleLowerCase()}.webp`}/>
-          <div style={item.read ? {} : read.read}>
+          <div style={item.read ? {} : read.read}> {}
+          
+
           <p className='notif-text'>
             <span id="author">{item.author}</span>
             {" "}
@@ -24,21 +39,30 @@ function App() {
               {item.type}
                </span>
 
-          {/* <span>{item.type}</span> */}
+          
             {" "}
             {item.content.includes(".webp") ? (
             <img src={item.content} alt="Notification Content" />
               ) : (
               
-             <span style={{ color: item.type === "Left the group" || item.type === "has joined your group" 
-               ? "#0A327B"  : "#5E6778", fontWeight: 600  }}>
-                 {item.content}
+             <span id="content"style={{ color: item.type === "Left the group" || item.type === "has joined your group" 
+               ? "#0A327B"  : "#5E6778", fontWeight: 600, ...(item.content.includes("Hello") ? bg.bg : {})
+               
+               }}>
+                
+                {item.content}
                    </span>
                   )}
+                  
                  
-
+                  
+                    {/* <span style={circle.circle}>...</span> */}
+                    {item.read === false ? <span style={circle.circle}>...</span>
+                    : null}
+                   
             <p id='time'>{item.time}</p>
           </p>
+          
           </div>
         </div>
         
@@ -46,10 +70,12 @@ function App() {
       })}
     </div>
    </div>
+ 
    
   
      
   )
+ 
 }
 const styles ={
   type: {
@@ -63,6 +89,41 @@ const read = {
     borderRadius:"10px",
   }
 }
+const bg = {
+  '@media (min-width: 768px)':{
+  bg:{
+    // backgroundColor: '#E5EFFA',
+    // backgroundClip:' initial',
+    // backgroundColor: 'rgb(255, 255, 255)',
+       paddingTop:' 17px',
+       paddingRight: '25px',
+       paddingBottom:' 22px',
+       paddingLeft: '10px',
+       color: 'rgb(94, 103, 120)',
+       fontSize: '1rem',
+       fontWeight:' 500',
+       lineHeight: '1.25rem',
+  }
+
+  },
+  bg:{
+    color:'#5E6778',
+    fontSize:'12px',
+    fontWeight:'500',
+    border:' 1px solid #DDE7EE',
+    }
+}
+
+const circle = {
+  circle:{
+    backgroundColor:'red',
+    borderRadius:'50%',
+    color:'red',
+    fontSize:'8px',
+    marginLeft:'3px',
+  }
+}
+
 
 
 export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import './Header.css'
+const markAsRead = () => {
+  const clone = userData.map(item => {
+    item.read = true;
+    return item;
+  })
+  console.log(clone);
+
+}
 
 
 
-function Header(){
+function Header({markAsRead}){
     return(
       <div className='header'>
         <h1 className='logo'>Notifications <span className='notif-quantity'>3</span></h1>
-        <h6 className='marking'>Mark as all read</h6>
+        <h6 onClick={markAsRead}className='marking'>Mark as all read</h6>
       </div>
     )
 }
